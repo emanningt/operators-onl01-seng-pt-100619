15cdc3891dce00d4db443f2177fc6b2285afcd9f
Eric Manning <emanningmusic@yahoo.com>
2019-10-12 00:05:24 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,11 @@
 def unsafe?(speed)
-  if speed > 40
-    return true
-  elsif speed < 40 
-    return true
-  else speed > 30
-    return false
-  end
+  
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
 end
 	
 
